e3fc0612fd5670ab3420b9dc85eaf9afde6fbc63
B1659 <B1659@BSP01045.bsp.co.jp>
2016-03-18 13:04:13 +0900

    コミット

---
+++ src/jp/co/iscala/HelloWorld.scala
@@ -0,0 +1,16 @@
+package jp.co.iscala
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+
+    println("""aaaaaaaa
+bbbbbbbbbbbbb
+\\tddddddddddd
+ccccccccccccccccccccccccc""");
+
+    var aaa = "aaaaa";
+    aaa = "";
+
+  }
+}
